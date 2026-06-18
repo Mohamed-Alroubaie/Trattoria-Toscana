48df31ca08a38cb0271524ad5a9298af91336c62
Mohamed <alroubaiemohamed@gmail.com>
2026-06-18 15:43:44 +0200

    change Next Config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  output: 'export', // Tells Next.js to build a static site
+  images: {
+    unoptimized: true, // Needed because GitHub Pages does not support image optimization
+  },
 };
 
 export default nextConfig;
